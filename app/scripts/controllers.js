a1cf4039a0e45b14fb8016b2d98f97cf6d3c9d51
Braxton Diggs <braxtondiggs@gmail.com>
2015-11-05 01:45:59 -0500

    Fix Intro Looks, Fix Back Button & Cache Issue

--- app/scripts/controllers.js
+++ app/scripts/controllers.js
@@ -1,6 +1,12 @@
 'use strict';
 angular.module('abra.controllers', [])
 
-.controller('AppCtrl', [function() {
-
+.controller('AppCtrl', ['$window', '$state', '$ionicPlatform', function($window, $state, $ionicPlatform) {
+	$ionicPlatform.registerBackButtonAction(function() {
+		if ($state.current.name === 'app.intro') {
+			navigator.app.exitApp();
+		} else {
+			$window.location = '#/app/intro/';
+		}
+	}, 100);
 }]);
--- app/scripts/intro.js
+++ app/scripts/intro.js
@@ -1,32 +1,41 @@
 'use strict';
 /*global Parse*/
 angular.module('abra.controllers')
-	.controller('IntroCtrl', ['$scope', '$window', '$interval', function($scope, $window, $interval) {
-        $scope.count = null;
-        var Rappers = Parse.Object.extend('Rappers');
-        function findImage() {
-            var query = new Parse.Query(Rappers);
-            query.limit(1);
-            query.skip(Math.floor(Math.random() * $scope.count));
-            query.find({
-                success: function(results) {
-                    $scope.introImg = results[0].get('image')._url;
-                    $scope.$apply();
-                }
-            });
-        }
-        var query = new Parse.Query(Rappers);
-        query.count({
-            success: function(count) {
-                $scope.count = count;
-                findImage();
-            }
-        });
-        $interval(function() {
-            findImage();
-        }, 5000);
+	.controller('IntroCtrl', ['$scope', '$window', '$interval', '$timeout', function($scope, $window, $interval , $timeout) {
+		$scope.count = null;
+		$scope.isChanged = false;
+		var Rappers = Parse.Object.extend('Rappers');
+		function findImage() {
+			var query = new Parse.Query(Rappers);
+			query.limit(1);
+			query.skip(Math.floor(Math.random() * $scope.count));
+			query.find({
+				success: function(results) {
+					$scope.introImg = results[0].get('image')._url;
+					$timeout(function() {
+						$scope.isChanged = true;
+						$scope.$apply();
+					}, 500);
+				}
+			});
+		}
+		var query = new Parse.Query(Rappers);
+		query.count({
+			success: function(count) {
+				$scope.count = count;
+				findImage();
+			}
+		});
+		var interval = $interval(function() {
+			$scope.isChanged = false;
+			findImage();
+		}, 5000);
 
-        $scope.go = function() {
-            $window.location = '#/app/main';
-        };
+		$scope.go = function() {
+			if (angular.isDefined(interval)) {
+				$interval.cancel(interval);
+				interval = undefined;
+			}
+			$window.location = '#/app/main';
+		};
 	}]);
